c07dec49eafa72591f71a18609de5e8587ff03ad
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-04-12 11:10:08 -0300

    função de desmarcar e marcar os itens da lista

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 
 import { useState } from 'react'
-import { findByDisplayValue } from '@testing-library/react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
   const [todos, setTodos] = useState([{
-    id: '1',
-    todo: 'ok'
+    id: 1,
+    todo: 'Adcione um todo',
+    isDone: false
   }])
-
+  
   function handerAdd(e) {
 
     e.preventDefault()
@@ -19,14 +20,24 @@ function App() {
       return
 
     }
-    setTodos([...todos, { id: value, todo: value }])
+    setTodos([...todos, { id: uuidv4(), todo: value, isDone: false }])
     e.target.todo.value = ''
 
     console.log(todos)
 
   }
+  
   function handleDone(id) {
-    console.log(id)
+    const newTodos = todos.map(
+      item => {
+        if (item.id === id) {
+          item.isDone = !item.isDone
+        }
+        return item
+      }   
+    )  
+    setTodos(newTodos)
+
   }
   function removeList(id) {
     const newTodos = todos.filter(item => item.id !== id)
@@ -44,19 +55,25 @@ function App() {
           </div>
           <form onSubmit={handerAdd}>
             <div id='box_input'>
-              <input id="todo" placeholder='  Add Todo'/>
+              <input id="todo" placeholder='Add Todo' />
               <button id='add'>ADD</button>
             </div>
             {
               todos.map(todo =>
                 <div id='box_list' key={todo.id}>
-                  <p id='lista'>
+
+                  <p id='lista'  style={
+                    todo.isDone ? doneCheck : notDoneCheck
+                  }
+                  >
                     {
                       todo.todo
                     }
                   </p>
                   <div id='box_button'>
-                    <button id='done' onClick={() => handleDone(todo.id)}> done</button>
+                    <button id='done' onClick={() => handleDone(todo.id)}> {
+                      todo.isDone ? 'Not Done' : 'Done'
+                    }</button>
                     <button id='remove' onClick={() => removeList(todo.id)}> remove</button>
                   </div>
                 </div>
@@ -66,9 +83,17 @@ function App() {
         </div>
       </section>
     </body>
-
   );
 }
 
 export default App;
 
+
+const doneCheck = {
+  color: 'green',
+  textDecoration: 'line-through',
+}
+
+const notDoneCheck = {
+  color: '#000'
+}
